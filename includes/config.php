d6544c9ef85edfd0abe24f484e0cc9cf17fcebcf
kapilsharma <kapil@kapilsharma.info>
2014-04-06 02:29:59 +0530

    index.php page with supporting files.

---
+++ includes/config.php
@@ -0,0 +1,13 @@
+<?PHP
+$config = array();
+
+$config['site_name'] = "PHP Reboot";
+
+$config['site_url'] = "http://local.phpreboot.com";
+$config['image_url'] = $config['site_url'] . '/images';
+$config['css_url'] = $config['site_url'] . '/css';
+$config['js_url'] = $config['site_url'] . '/js';
+
+$config['path'] = 'D:\projects\git\phpreboot' . DIRECTORY_SEPARATOR;
+$config['os_path'] = $config['path'] . "www" . DIRECTORY_SEPARATOR;
+$config['include_path'] = $config['path'] . "includes" . DIRECTORY_SEPARATOR;
