afc3f8d3e577c503276bf65af80f74fcfc106412
kapilsharma <kapil@kapilsharma.info>
2014-11-09 14:52:26 +0530

    Deleted old template files

--- includes/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <div class="wrap-footer">
-        <div class="copyright">
-            <p>Copyright © 2014 PHP Reboot - Template by <a href="http://www.zerotheme.com" target="_blank">Zerotheme.com</a></p>
-        </div>
-    </div>
-</footer>
-
-</body></html>
