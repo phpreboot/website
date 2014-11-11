f31e5681eb1d30c17f0c492f604ea6ee31741e36
kapilsharma <kapil@kapilsharma.info>
2014-11-11 18:51:57 +0530

    Not complete

---
+++ includes/footer.php
@@ -0,0 +1,90 @@
+    <div id="footer">
+        
+        <div class="container">
+            
+            <div id="footer_about" class="footer_info">
+                
+                <h4>about us</h4>
+                <p>
+                    PHP Reboot is an attempt to share news and new features of PHP and related
+                    technologies. We do this by sharing articles, news and tutorials through
+                    free PHP Reboot Magazine and blogs.
+                </p>
+                
+            </div> <!-- END #footer_about -->
+            
+            <div id="footer_explore" class="footer_info">
+                
+                <h4>explore</h4>
+                <ul>
+                    <li><a href="index.html">home</a></li>
+                    <li><a href="#">about us</a></li>
+                    <li><a href="#">services</a></li>
+                    <li><a href="portfolio.html">portfolio</a></li>
+                    <li><a href="#">blog</a></li>
+                </ul>
+                
+            </div> <!-- END #footer_about -->
+            
+            <div id="footer_browse" class="footer_info">
+                
+                <h4>browse</h4>
+                <ul>
+                    <li><a href="#">careers</a></li>
+                    <li><a href="#">press &amp; media</a></li>
+                    <li><a href="contact.html">contact us</a></li>
+                    <li><a href="#">terms of service</a></li>
+                    <li><a href="#">privacy policy</a></li>
+                </ul>
+                
+            </div> <!-- END #footer_about -->
+            
+            <div id="footer_contact" class="footer_info">
+                
+                <h4>contact us</h4>
+                <p><span class="bold_text">BisLite Inc.</span>
+                <br />
+                Always Street 265
+                <br />
+                0X - 125 - Canada
+                <br />
+                <br />
+                Phone: 987-6543-210
+                <br />
+                Fax: 987-6543-210</p>
+                
+            </div> <!-- END #footer_about -->
+            
+            <div id="footer_connect" class="footer_info">
+                
+                <h4>connect with us</h4>
+                
+                <ul>
+                    <li><a href="#" id="facebook" title="Facebook">Facebook</a></li>
+                    <li><a href="#" id="dribbble" title="Dribbble">Dribbble</a></li>
+                    <li><a href="#" id="pinterest" title="Pinterest">Pinterest</a></li>
+                    <li><a href="#" id="linkedin" title="LinkedIn">LinkedIn</a></li>
+                    <li><a href="#" id="skype" title="Skype">Skype</a></li>
+                    <li><a href="#" id="sharethis" title="Share This">ShareThis</a></li>
+                </ul>
+                
+            </div> <!-- END #footer_about -->
+            
+            <p id="copyright">
+                &copy; Copyright 2014 - PHP Reboot. All the contents of this site are copyright of
+                PHP Reboot.
+                <br />
+                Blog post in magazine is just for knowledge sharing. They are copyright
+                property of original site/authors.
+            </p>
+            
+            <a href="index.html" id="footer_logo">
+                <img src="<?= $config['image_url']; ?>/logo.png" width="140px" height="35px">
+            </a>
+        
+        </div> <!-- END .container -->
+        
+    </div> <!-- END #footer -->
+    
+</body>
+</html>
