b3741f59b55b6f4680be5050324b25de33223209
kapilsharma <kapil@kapilsharma.info>
2016-07-02 15:40:08 +0530

    Removed old site folders

--- includes/download.php
+++
@@ -1,8 +0,0 @@
-        <div id="download">
-            <p>
-                PTP Reboot magazine for October 2014 is now freezed. Add it to your Flipboard to
-                read all the PHP and related technologies articles, blogs, tutorials on your
-                mobile and tablets. You can also check it on browser.
-             </p>
-            <a href="<?= $config['site_url'] ?>/magazine/october14.php">Add To Flipboard</a>
-        </div> <!-- END #download -->
