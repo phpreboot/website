7dca8eef619db89bad8f97c4387304c39bfdbdc2
kapilsharma <kapil@kapilsharma.info>
2016-07-04 23:15:21 +0530

    Home page

--- resources/views/PhpReboot/Home/homePage.blade.php
+++ resources/views/PhpReboot/Home/homePage.blade.php
@@ -3,5 +3,20 @@
 @section('title', 'Home')
 
 @section('content')
-    <p>This is my body content.</p>
+    <div class="box box-default color-palette-box">
+        <div class="box-header with-border">
+            <h3 class="box-title"><i class="fa fa-tag"></i> What we do?</h3>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 col-md-4">
+                hi
+            </div>
+            <div class="col-sm-12 col-md-4">
+                hi
+            </div>
+            <div class="col-sm-12 col-md-4">
+                hi
+            </div>
+        </div>
+    </div>
 @endsection
