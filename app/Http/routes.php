a7231b2b1c50d106c34e4fa555d50fb5c152c391
kapilsharma <kapil@kapilsharma.info>
2016-08-20 16:42:32 +0530

    Added routes for password

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,12 +4,11 @@
 |--------------------------------------------------------------------------
 | Static site pages of PHP Reboot
 |--------------------------------------------------------------------------
-|
 | Here is where we will define common website pages like home page, about,
 | team, faqs etc.
 |
 | They will be placed in Controllers/PhpReboot.
-|
+|--------------------------------------------------------------------------
 */
 Route::get('/', [
     'as' => 'home',
@@ -25,11 +24,10 @@ Route::get('/meetup', [
 |--------------------------------------------------------------------------
 | Authentication routes for PHP Reboot
 |--------------------------------------------------------------------------
-|
 | Below are the authentication and registration routes for
 | PHP Reboot. They are Laravel 5.1 default authentication
 | and registration routes.
-|
+|--------------------------------------------------------------------------
 */
 // Authentication routes
 Route::get('auth/login', 'Auth\AuthController@getLogin');
@@ -38,3 +36,9 @@ Route::get('auth/logout', 'Auth\AuthController@getLogout');
 // Registration routes...
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
+// Password reset link request routes...
+Route::get('password/email', 'Auth\PasswordController@getEmail');
+Route::post('password/email', 'Auth\PasswordController@postEmail');
+// Password reset routes...
+Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
+Route::post('password/reset', 'Auth\PasswordController@postReset');
