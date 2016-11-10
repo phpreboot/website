fb8864c2cf3a3c9d724edaebb813c68cbcc170db
Sahil Sharma <sahilsharma011@users.noreply.github.com>
2016-11-10 19:42:15 +0530

    Fixed a very minor typo

--- database/queries/manual_queries.sql
+++ database/queries/manual_queries.sql
@@ -5,7 +5,7 @@
 -- issue - Hotfix 161.
 update websites set noc='remove' where site_url = 'alanstorm.com';
 
--- Based on other permissions (approcal.txt) as of 10th November 2016.
+-- Based on other permissions (approval.txt) as of 10th November 2016.
 update websites set noc='iframe' where site_url = 'codeutopia.net';
 update websites set noc='iframe' where site_url = 'ma.ttias.be';
 update websites set noc='link' where site_url = 'sitepoint.com';
